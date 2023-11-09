2acc7511b30fff846ae6aec054ed21e91281567d
Michelet Saint-Preux <michelet2000@gmail.com>
2023-11-09 09:09:40 +0800

    app.js index.html

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-
-
-
-let myLeads = []
+let myLeads = ["www.google.com", "www.yahoo.com", "www.facebook.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+/*
+for (let i = 0; i < myLeads.length; i++) {
+    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+    
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
+    
+}
+*/
+
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>"
+}
+
+ulEl.innerHTML = listItems
