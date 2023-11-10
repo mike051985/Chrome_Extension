e2c43a456c454d72e5c1b0f20bc2439d8cd7ed97
Michelet Saint-Preux <michelet2000@gmail.com>
2023-11-10 20:25:32 +0800

    app.js index.html style.css

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     // Clear out the input field
     inputEl.value = ""
+
+    // Save the myLeads array to localStorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
